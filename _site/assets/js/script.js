819cb116e11c5617660e59fd9e5a2fc9a6a07ae6
unknown <kreilly253@gmail.com>
2017-11-07 17:40:46 -0700

    g psi implemenation

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -1,5 +1,10 @@
 $('.pagespeed-form').on('submit', function(e){
 	e.preventDefault();
+	var height = $(this).parent().height();
+	$(this).parent().css({'minHeight':height});
+
+	$(this).fadeOut();
+	
 	var test_url = $(this).find('.test_url').val();
 	if(!test_url.includes('http')){
 		test_url = 'http://'+ test_url;
